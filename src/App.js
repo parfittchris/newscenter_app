377f2706c2fe8e7ddae5b72e6871500227611689
Chris Parfitt <parfitt.chris7@gmail.com>
2020-04-21 19:12:17 -0400

    update to use other api

--- src/App.js
+++ src/App.js
@@ -6,70 +6,128 @@ import './App.css';
 class App extends Component {
   constructor() {
     super()
-    this.state = {};
+    this.state = {
+      selected: []
+    };
+
+    this.orgSelector = this.orgSelector.bind(this);
   }
 
   componentDidMount() {
     axios
-      .get('https://newscenter-api.herokuapp.com/article/')
+      .get('https://newscenterapi.herokuapp.com/')
       .then(res => {
         let articles = res.data;
 
-        let cnnArticles = [];
         let foxArticles = [];
-        let nbcArticles = [];
+        let apArticles = [];
         let nytArticles = [];
-        let huffArticles = [];
+        let politicoArticles = [];
+        let reutersArticles = [];
+        let buzzfeedArticles = [];
+
         
         for (let i = 0; i < articles.length; i++) {
             const article = articles[i];
 
             switch(article.site) {
-              case 'CNN':
-                cnnArticles.push(article);
-                break;
-              case 'FOX':
+              case 'Fox':
                 foxArticles.push(article);
                 break;
-              case 'NBC News':
-                nbcArticles.push(article);
+              case 'AP':
+                apArticles.push(article);
                 break;
-              case 'Huffington Post':
-                huffArticles.push(article);
+              case 'Politico':
+                politicoArticles.push(article);
                 break;
-              case 'NYTimes':
+              case 'nytimes':
                 nytArticles.push(article);
+                break;
+              case 'Reuters':
+                reutersArticles.push(article);
+                break;
+              case 'Buzzfeed':
+                buzzfeedArticles.push(article);
                 break;
               default:
                 break;
             }
 
-            this.setState({
-              cnnArticles,
-              foxArticles,
-              nbcArticles,
-              huffArticles,
-              nytArticles
+          this.setState({
+            allArticles: {
+                foxArticles,
+                apArticles,
+                politicoArticles,
+                nytArticles,
+                reutersArticles,
+                buzzfeedArticles
+              }
             });
-        }
+          }
       })
       .catch(err => {
         console.log(err);
       });
   }
 
+  orgSelector(e) {
+    let orgs = this.state.selected;
+
+    if (e.target.getAttribute('pressed') === 'false') {
+      e.target.setAttribute('pressed', 'true')
+      e.target.classList.remove('choiceBtn');
+      e.target.classList.add('choiceBtnActive');
+
+      const org = {
+        name: e.target.getAttribute('name'),
+        articles: e.target.getAttribute('value') + 'Articles'
+      }
+      orgs.push(org)
+    } else {
+      orgs = orgs.filter(el => el.name !== e.target.getAttribute('name'))
+      e.target.setAttribute('pressed', 'false')
+      e.target.classList.remove('choiceBtnActive');
+      e.target.classList.add('choiceBtn');
+    }
+
+    this.setState({
+      selected: orgs
+    });
+  }
+
+  populateBoard() {
+    const orgs = this.state.selected;
+    return orgs.map((el, idx) => {
+      return <NewsBanner key={idx} site={el.name} articles={this.state.allArticles[el.articles]} className="test"/>
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <h1 id="app_header">News Center</h1>
-        <div className="articles_container">
-          <NewsBanner site='FOX' articles={this.state.foxArticles} />
-          <NewsBanner site='CNN' articles={this.state.cnnArticles}/>
-          <NewsBanner site='NBC News' articles={this.state.nbcArticles}/>
+        <div className="articlesContainer">
+          <div id="pickerSection">
+            <div className="pickerItems">
+              <h3 className="pickerTitle">Select News Sources</h3>
+              <div className="choices">
+                <div className="choiceBtn" onClick={this.orgSelector} pressed="false" value="fox" name="Fox">Fox News</div>
+                <div className="choiceBtn" onClick={this.orgSelector} pressed="false" value="reuters" name="Reuters">Reuters</div>
+                <div className="choiceBtn" onClick={this.orgSelector} pressed="false" value="buzzfeed" name="Buzzfeed">Buzzfeed</div>
+                <div className="choiceBtn" onClick={this.orgSelector} pressed="false" value="ap" name="AP">AP News</div>
+                <div className="choiceBtn" onClick={this.orgSelector} pressed="false" value="politico" name="Politico">Politico</div>
+                <div className="choiceBtn" onClick={this.orgSelector} pressed="false" value="nyt" name="New York Times">NY Times</div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h1 id="app_header">News Center</h1>
+            <div className="articlesSection">
+              {this.populateBoard()}
+            </div>
+          </div>
         </div>
         <div className="articles_container">
-          <NewsBanner site='NYTimes' articles={this.state.nytArticles}/>
-          {/* <NewsBanner site='Huffington Post' articles={this.state.huffArticles}/> */}
         </div>
       </div>
     );
--- src/Components/NewsBanner/newsBanner.jsx
+++ src/Components/NewsBanner/newsBanner.jsx
@@ -13,22 +13,32 @@ class NewsBanner extends Component {
         window.open(path);
     };
 
+    componentDidMount() {
+        this.getArticles()
+    }
+
+    componentWillUnmount() {
+        this.setState({
+
+        })
+    }
+
     getArticles() {
         const tableId = 'articleTable' + this.props.site;
         const table = document.getElementById(tableId);
-        
-        if (table) table.innerHTML = "";
-        
-        if (this.props.articles) {
-            for (let i = 0; i < this.props.articles.length; i++) {
-                if (i === 15) break;
-                const article = this.props.articles[i];
-                let newRow = table.insertRow();
-                newRow.innerHTML = `<td href=${article.url}>${article.title}</td>`;
-                newRow.classList.add('articleLink');
-                newRow.onclick = this.routeChange;
+        if (table) {
+            table.innerHTML = "";
+            if (this.props.articles) {
+                for (let i = 0; i < this.props.articles.length; i++) {
+                    if (i === 15) break;
+                    const article = this.props.articles[i];
+                    let newRow = table.insertRow();
+                    newRow.innerHTML = `<td href=${article.url}>${article.title}</td>`;
+                    newRow.classList.add('articleLink');
+                    newRow.onclick = this.routeChange;
+                };
             };
-        };
+        }
     }
 
     render() {
